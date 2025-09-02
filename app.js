929989d2d1a804b8a71f8740ed73ddd434ad264f
Samavia-siddiqui <samaviasiddiqui3@gmail.com>
2025-09-02 13:28:11 +0500

    Done

---
+++ app.js
@@ -0,0 +1,31 @@
+//question 1
+var username
+//question 2
+var myName = "Samavia Siddiqui"
+//question 3
+var message = "Hello World"
+alert(message)
+//question 4
+var name = "Samavia Siddiqui"
+var age = "18 years old"
+var course = "Certified Mobile Application Development"
+alert(name)
+alert(age)
+alert(course)
+//question 5
+var pizza = "PIZZA \nPIZZ \nPIZ \nPI \nP"
+alert(pizza)
+//question 6
+var email = "samaviasiddiqui3@gmail.com"
+alert("My email address is " + email)
+//question 7
+var book = "A smarter way to learn JavaScript"
+alert("I am trying to learn from the book " + book)
+//question 8
+document.write("Yah! I can write HTML content through JavaScript")
+//question 9
+var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+
+alert(design)
+
+document.write(design)
