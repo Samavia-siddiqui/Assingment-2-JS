ae2c6fc418727cc0a5fa225cb3c88e4db43a3b36
Samavia-siddiqui <samaviasiddiqui3@gmail.com>
2025-09-04 12:28:22 +0500

    done

--- assignment03/app.js
+++ assignment03/app.js
@@ -1,23 +1,14 @@
-// activity
-
-
-// userName;
-
-// firstnameLastName;
-
-// emailAddressValidation;
-
-// getUserById;
-
-// calculateTotalAmount;
-
-// isValidInput;
-
-// conertToJsonString;
-
-// httpResponseCode;
-
-// xmlHttpRequest;
-
-// databaseConnectionPool;
-
+// question01
+let age = 18
+alert("i am " + age + " years old")
+// queston02
+let visit = 7
+alert("you have visited this site " + visit + " times")
+// question03
+// let birthYear = 2006
+// document.write("my birth year is " + birthYear + "<br>" + " data type of my declared variable is number")
+// question04
+let visitorName = "sammi khan"
+let productTitle = "shoes"
+let quantity = 5
+document.write(visitorName + " ordered " + quantity + " " + productTitle + "(s) on samavia clothing store")
