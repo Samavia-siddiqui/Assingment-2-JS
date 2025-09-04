ef0b6241126f6ded13841185f8f4eb735ce23d25
Samavia-siddiqui <samaviasiddiqui3@gmail.com>
2025-09-04 12:48:19 +0500

    done

---
+++ assignment04/app.js
@@ -0,0 +1,31 @@
+// question01
+let a ="hello world", b= "lorem ipsum", c="javascript is cool";
+// question02
+// Legal Variable Names
+// myVar
+
+// _count
+
+// total1
+
+// user_name
+
+// Age
+
+// Illegal Variable Names
+// 1stValue (starts with a digit)
+
+// my-var (contains a hyphen, which is not allowed)
+
+// user name (contains a space)
+
+// class (reserved keyword)
+
+// total$ (contains a special character $ not allowed in some languages)
+
+// question03
+document.write("<h1>Rules for naming JS variables</h1>");
+document.write("Variable names can only contain letters, digits, underscores, and dollar signs. For example: $my_1stVariable<br>");
+document.write("Variables must begin with a letter, underscore, or dollar sign. For example: $name, _name or name<br>");
+document.write("Variable names are case sensitive<br>");
+document.write("Variable names should not be JS keywords<br>");
