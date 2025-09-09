b570f2b7ae9802110eba0f661ce018537fafb259
Shahmeer <diarydear313@gmail.com>
2025-09-09 12:45:28 +0500

    done 5

---
+++ assignment05/app.js
@@ -0,0 +1,92 @@
+// question01
+// let a =3;
+// let b =5;
+// let c = a + b;
+// document.write  (`Sum of 3 and 5 is ${c}`);
+// // question02
+// let d = a - b;
+// document.write (`<br> Subtraction of 3 and 5 is ${d}`);
+// let e = a * b;
+// document.write (`<br> Multiplication of 3 and 5 is ${e}`);
+// let f = a / b;
+// document.write (`<br> Division of 3 and 5 is ${f}`);
+// let g = a % b;
+// document.write (`<br> Modulus of 3 and 5 is ${g}`);
+// question03
+let num;
+// document.write (`<br> Value after variable declaration is: ${num}`);
+// num = 5;
+// document.write (`<br> Initial value: ${num}`);
+// num++;
+// document.write (`<br> Value after increment is: ${num}`);
+// num = num + 7;
+// document.write (`<br> Value after addition is: ${num}`);
+// num--;
+// document.write (`<br> Value after decrement is: ${num}`);
+// num = num % 3;
+// document.write (`<br> The remainder is: ${num}`);
+// question04
+// let ticketPrice = 600;
+// let totalCost = ticketPrice * 5;
+// document.write (`<br> Total cost to buy 5 tickets to a movie is ${totalCost}PKR`);
+// question06;
+// let celsius = 25;
+// let fahrenheit = (celsius * 9/5) + 32;
+// document.write (`<br> ${celsius}°C is ${fahrenheit}°F`);
+// let fahrenheitTemp = 70;
+// let celsiusTemp = (fahrenheitTemp - 32) * 5/9;
+// document.write (`<br> ${fahrenheitTemp}°F is ${celsiusTemp}°C`);
+// question07
+// let item1Price = 650;
+// let item2Price = 100;
+// let item1Quantity = 3;
+// let item2Quantity = 7;
+// let shippingCharges = 100;
+// let totalCost = (item1Price * item1Quantity) + (item2Price * item2Quantity) + shippingCharges;
+// document.write (`<br> Price of item 1 is ${item1Price}`);
+// document.write (`<br> Quantity of item 1 is ${item1Quantity}`);
+// document.write (`<br> Price of item 2 is ${item2Price}`);
+// document.write (`<br> Quantity of item 2 is ${item2Quantity}`);
+// document.write (`<br> Shipping Charges ${shippingCharges}`);
+// document.write (`<br> Total cost of your order is ${totalCost}`);
+// question08
+// let totalMarks = 980;
+// let marksObtained = 804;
+// let percentage = (marksObtained / totalMarks) * 100;
+// document.write (`<br> Total Marks: ${totalMarks}`);
+// document.write (`<br> Marks Obtained: ${marksObtained}`);
+// document.write (`<br> Percentage: ${percentage}%`);
+// question09
+// let usDollarToPkr = 104.80;
+// let saudiRiyalToPkr = 28;
+// let totalCurrencyInPkr = (10 * usDollarToPkr) + (25 * saudiRiyalToPkr);
+// document.write (`<br> Total Currency in PKR: ${totalCurrencyInPkr}`);
+// // question10
+// let numb= 10;
+// let result = ((numb + 5) * 10) / 2;
+// document.write (`<br> Result: ${result}`);
+// question11
+// let currentYear = 2025;
+// let birthYear = 2006;
+// let age = currentYear - birthYear;
+// document.write (`<br> Current Year: ${currentYear}`);
+// document.write (`<br> Birth Year: ${birthYear}`);
+// document.write (`<br> Your Age is: ${age}`);
+// question12
+// let radius = 20;
+// // let circumference = 2 * 3.142 * radius;
+// let area = 3.142 * radius * radius;
+// document.write (`<br> Radius of a circle: ${radius}`);
+// document.write (`<br> The circumference is: ${circumference}`);
+// document.write (`<br> The area is: ${area}`);
+// question13
+// let favSnack = "Chocolate Chip";
+// let currentAge = 18;
+// let maxAge = 65;
+// let estAmountPerDay = 3;
+// let totalNeeded = (maxAge - currentAge) * estAmountPerDay;
+// document.write (`<br> Favourite Snack: ${favSnack}`);
+// document.write (`<br> Current Age: ${currentAge}`);
+// document.write (`<br> Estimated Maximum Age: ${maxAge}`);
+// document.write (`<br> Amount of snacks per day: ${estAmountPerDay}`);
+// document.write (`<br> You will need ${totalNeeded} ${favSnack} to last you until the ripe old age of ${maxAge}`);
