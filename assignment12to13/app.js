245ae6161d66af546822bec2e032dc0f6c4133d9
Samavia-siddiqui <samaviasiddiqui3@gmail.com>
2025-09-25 10:37:57 +0500

    done 2

--- assignment12to13/app.js
+++ assignment12to13/app.js
@@ -1,3 +1,9 @@
-for(i=1; i<=15 ;i++)
-{console.log(i);
+// for(i=1; i<=15 ;i++)
+// {console.log(i);
+//     document.write( i);
+// }
+for( let i=1; i<=10 ; i++) {
+    if ( i % 0 ) ; 
+    else  " this is not an even number"
+    console.log( i )
 }
